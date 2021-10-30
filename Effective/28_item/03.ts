7bca2a113698f12fbf8de74d0501847026a22ba6
polaris1009 <lofend1009@gmail.com>
2021-10-31 02:36:24 +0900

    6th day

---
+++ Effective/28_item/03.ts
@@ -0,0 +1,10 @@
+{
+  interface State {
+    pageText: string;
+    isLoading: boolean;
+    error?: string;
+  }
+  declare let currentPage: string;
+  function getUrlForPage(p: string) { return ''; }
+  
+}
